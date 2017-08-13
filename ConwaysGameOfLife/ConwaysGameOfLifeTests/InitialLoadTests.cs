ed52793c7d160bc2df03f752feeaac6743277e08
Joshua Warren <jwarren@pryor.com>
2017-08-13 18:12:20 -0500

    write a couple simple tests

--- ConwaysGameOfLife/ConwaysGameOfLifeTests/InitialLoadTests.cs
+++ ConwaysGameOfLife/ConwaysGameOfLifeTests/InitialLoadTests.cs
@@ -1,7 +1,19 @@
-﻿namespace ConwaysGameOfLifeTests
+﻿using NUnit.Framework;
+
+namespace ConwaysGameOfLifeTests
 {
     public class InitialLoadTests
     {
+        [Test]
+        public void It_should_create_a_list_of_rows_50_cells_wide()
+        {
+            
+        }
 
+        [Test]
+        public void It_should_create_a_list_50_rows_long()
+        {
+            
+        }
     }
 }
