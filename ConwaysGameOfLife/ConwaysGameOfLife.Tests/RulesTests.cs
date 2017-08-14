927432ea13d9b777a48e82dd4a153b9105d5be66
Joshua Warren <jwarren@pryor.com>
2017-08-13 19:26:33 -0500

    fill out them rules tests

--- ConwaysGameOfLife/ConwaysGameOfLife.Tests/RulesTests.cs
+++ ConwaysGameOfLife/ConwaysGameOfLife.Tests/RulesTests.cs
@@ -24,26 +24,36 @@ namespace ConwaysGameOfLife.Tests
         [Test]
         public void It_should_die_if_it_has_4_neighbors()
         {
+            Subject.Judge(4);
+            Subject.IsAlive.ShouldBeFalse();
         }
 
         [Test]
         public void It_should_die_if_it_has_no_neighbors()
         {
+            Subject.Judge(0);
+            Subject.IsAlive.ShouldBeFalse();
         }
 
         [Test]
         public void It_should_populate_if_it_has_3_neighbors()
         {
+            Subject.Judge(3);
+            Subject.IsAlive.ShouldBeTrue();
         }
 
         [Test]
         public void It_should_survive_if_it_has_2_neighbors()
         {
+            Subject.Judge(2);
+            Subject.IsAlive.ShouldBeTrue();
         }
 
         [Test]
         public void It_should_survive_if_it_has_3_neighbors()
         {
+            Subject.Judge(3);
+            Subject.IsAlive.ShouldBeTrue();
         }
     }
 }
