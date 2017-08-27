5945830f8ba013bc0bdd62954ff7616eb7176c0f
Joshua Warren <jwarren@pryor.com>
2017-08-26 19:13:16 -0500

    remove unused usings

--- ConwaysGameOfLife/ConwaysGameOfLife.Tests/MatrixTests.cs
+++ ConwaysGameOfLife/ConwaysGameOfLife.Tests/MatrixTests.cs
@@ -1,9 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using AutoMoq.Helpers;
-using Castle.Components.DictionaryAdapter;
 using ConwaysGameOfLife.Models;
-using Moq;
 using NUnit.Framework;
 using Should;
 
