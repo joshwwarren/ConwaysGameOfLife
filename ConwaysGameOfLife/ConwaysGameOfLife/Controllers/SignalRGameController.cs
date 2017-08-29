ce7ea2c86c053ec3e5e7b37211cecbb982bd6fb0
Joshua Warren <jwarren@pryor.com>
2017-08-28 19:26:47 -0500

    new controller and view for signal r stuff

---
+++ ConwaysGameOfLife/ConwaysGameOfLife/Controllers/SignalRGameController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ConwaysGameOfLife.Controllers
+{
+    public class SignalRGameController : Controller
+    {
+        // GET: SignalRGame
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
