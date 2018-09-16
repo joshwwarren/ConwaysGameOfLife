cb1a072958f84a435c98bf60827af9e0dc039b2c
Joshua Warren <jwarren@pryor.com>
2018-09-16 16:47:31 -0500

    installed simpleinjector.integraiton.mvc adn got the sample code to work

--- ConwaysGameOfLife/ConwaysGameOfLife/Global.asax.cs
+++ ConwaysGameOfLife/ConwaysGameOfLife/Global.asax.cs
@@ -2,6 +2,9 @@
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using ConwaysGameOfLife.Models.Matrix;
+using SimpleInjector;
+using SimpleInjector.Integration.Web.Mvc;
 
 namespace ConwaysGameOfLife
 {
@@ -13,6 +16,21 @@ namespace ConwaysGameOfLife
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // 1. Create a new Simple Injector container
+            var container = new Container();
+
+            // 2. Configure the container (register)
+            // See below for more configuration examples
+            container.Register<Matrix, Matrix>(Lifestyle.Transient);
+            //container.Register<IUserRepository, SqlUserRepository>(Lifestyle.Singleton);
+
+            // 3. Optionally verify the container's configuration.
+            container.Verify();
+
+            // 4. Store the container for use by the application
+            DependencyResolver.SetResolver(
+                new SimpleInjectorDependencyResolver(container));
         }
     }
 }
