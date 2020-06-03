c13ab62002781230c82b1e0b24e59e9d099c78f5
Joshua Warren <jwarren@pryor.com>
2020-06-03 16:36:46 -0500

    remove comments from original setup example

--- ConwaysGameOfLife/ConwaysGameOfLife/Global.asax.cs
+++ ConwaysGameOfLife/ConwaysGameOfLife/Global.asax.cs
@@ -18,13 +18,8 @@ namespace ConwaysGameOfLife
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            // 1. Create a new Simple Injector container
             var container = new Container();
 
-            // 2. Configure the container (register)
-            // See below for more configuration examples
-            //container.Register<IMatrix, Matrix>();
-            //container.Register<IUserRepository, SqlUserRepository>(Lifestyle.Singleton);
             var registrations =
                 from type in typeof(Game).Assembly.GetExportedTypes()
                 where type.Namespace.StartsWith("ConwaysGameOfLife.Models")
@@ -33,10 +28,9 @@ namespace ConwaysGameOfLife
 
             foreach (var reg in registrations)
                 container.Register(reg.Service, reg.Implementation);
-            // 3. Optionally verify the container's configuration.
+
             container.Verify();
 
-            // 4. Store the container for use by the application
             DependencyResolver.SetResolver(
                 new SimpleInjectorDependencyResolver(container));
         }
